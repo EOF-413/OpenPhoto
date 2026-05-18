b0c54254ded10e9487fb82882279202b5c3e5245
Eof413 <org.anime@mail.ru>
2026-05-18 20:25:31 +0300

    added comments

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from PyQt6.QtWidgets import QApplication, QWidget  #source -> python 3.14.5.
+from PyQt6.QtWidgets import QApplication, QWidget  # source -> python 3.14.5.
 
-app = QApplication([])
+app = QApplication([])  #* Цикл событий
 
 window = QWidget()
 window.show()
